58d45d931bf9d9b94666268bf57787b8cf373b12
jackspirou <jack.spirou@target.com>
2015-04-18 17:55:39 -0500

    changing to install.sh

--- initcfg.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
- 
-#
-# install:
-#   curl -sL https://raw.githubusercontent.com/initcgf/yosemite/master/initcfg.sh | bash
-#
-
-echo "initalizing Yosemite configuration - https://github.com/initcng/yosemite"
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#
+# install:
+#   curl -sL https://raw.githubusercontent.com/initcgf/yosemite/master/install.sh | bash
+#
+
+echo "installing Yosemite configuration - https://github.com/initcng/yosemite"
