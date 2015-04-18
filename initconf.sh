795ecef363de174fe1d73ce24cb67014f9ae3ecd
jackspirou <jack.spirou@me.com>
2015-04-18 15:57:10 -0500

    init commit

---
+++ initconf.sh
@@ -0,0 +1,6 @@
+# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# brew install brew-cask
+# brew install wget
+# wget https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg -P ~/Downloads/
+open ~/Downloads/googlechrome.dmg
+sudo cp -r /Volumes/Google\ Chrome/Google\ Chrome.app /Applications/
