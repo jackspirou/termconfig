c1503c5756a469e22f9543d060ba1cf3001b8b2a
jackspirou <jack.spirou@target.com>
2015-04-18 16:32:24 -0500

    changing naming convention

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+ 
+#
+# install:
+#   curl -sL https://raw.githubusercontent.com/initcgf/yosemite/master/initcfg.sh | bash
+#
+
+echo "initalizing Yosemite configuration - https://github.com/initcng/yosemite"
--- initconf.sh
+++
@@ -1,4 +0,0 @@
-# brew install wget
-# wget https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg -P ~/Downloads/
-open ~/Downloads/googlechrome.dmg
-sudo cp -r /Volumes/Google\ Chrome/Google\ Chrome.app /Applications/
