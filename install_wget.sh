20787f5aef5d004292332e215fa47d5c5fd73f8f
jackspirou <jack.spirou@target.com>
2015-04-18 16:25:52 -0500

    adding wget

---
+++ install_wget.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+ 
+#
+# install:
+#   curl -sL https://raw.githubusercontent.com/initcgf/yosemite/master/install_wget.sh | bash
+#
+
+echo "installing wget - https://www.gnu.org/software/wget/"
+
+brew install wget
