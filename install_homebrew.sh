d638335999e4fdf98e950d5f72467ad8c09dcf98
jackspirou <jack.spirou@target.com>
2015-04-18 16:15:37 -0500

    adding brew install

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
  
 #
 # install:
 #   curl -sL https:/raw.github.com/gist/2108403/hack.sh | sh
 #
+
+echo "Installing homebrew - http://brew.sh"
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install brew-cask
