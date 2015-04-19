3e3a9382d37269a89c128a2483cdccddbbdfc9b7
jackspirou <jack.spirou@target.com>
2015-04-19 18:34:35 -0500

    adding starter content to restart.sh

---
+++ restart.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#
+# install:
+#   curl -sL https://raw.githubusercontent.com/initcgf/yosemite/master/restart.sh | bash
+#
+
+echo "restarting processes, applications, and machine"
