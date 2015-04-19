a354f4882484911e1d91ee9826352bf918d77e7f
jackspirou <jack.spirou@target.com>
2015-04-18 19:05:27 -0500

    adding oh-my-zsh install

---
+++ install_oh-my-zsh.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#
+# install:
+#   curl -sL https://raw.githubusercontent.com/initcgf/yosemite/master/install_oh-my-zsh.sh | bash
+#
+
+echo "installing oh-my-zsh - http://ohmyz.sh"
+
+curl -L https://raw.githubusercontent.com/initcfg/oh-my-zsh/master/tools/install.sh | sh
